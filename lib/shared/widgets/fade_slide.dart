d7d32d7ea4f2e7efd39e6f776d6fb6798d1ae5a9
vinu4545 <vinurocky1131@gmail.com>
2026-06-29 19:38:50 +0530

    feat(animation): add reusable fade and slide animation widget

---
+++ lib/shared/widgets/fade_slide.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class FadeSlide extends StatefulWidget {
+  final Widget child;
+  final Duration delay;
+
+  const FadeSlide({
+    super.key,
+    required this.child,
+    this.delay = Duration.zero,
+  });
+
+  @override
+  State<FadeSlide> createState() => _FadeSlideState();
+}
+
+class _FadeSlideState extends State<FadeSlide>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  late final Animation<double> _opacity;
+
+  late final Animation<Offset> _offset;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 700),
+    );
+
+    _opacity = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    );
+
+    _offset = Tween(
+      begin: const Offset(0, .15),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOut,
+      ),
+    );
+
+    Future.delayed(widget.delay, () {
+      if (mounted) {
+        _controller.forward();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _opacity,
+      child: SlideTransition(
+        position: _offset,
+        child: widget.child,
+      ),
+    );
+  }
+}
